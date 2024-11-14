8653d5207ae2ae78cfad16e3c788f2862ce984a5
Maksim Rud <makru150@mail.ru>
2024-11-14 21:40:50 +0300

    cicd

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,6 +2,9 @@
 dotnet test -- RunConfiguration.ReporterSwitch=allure
 ls
 ls bin/
+ls bin/Debug/
+ls bin/Debug/net8.0
+ls bin/Debug/net8.0/allure-results
 ls bin/Release/
 ls bin/Release/net8.0
 ls bin/Release/net8.0/allure-results
